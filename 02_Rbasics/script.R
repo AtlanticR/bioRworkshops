b153430e53e83910dc4db1c6b784396ef4eabc44
Clark Richards <clark.richards@gmail.com>
2017-10-20 13:21:07 -0300

    did our first workshop
    First "noob" workshop, working notes and history

---
+++ 02_Rbasics/script.R
@@ -0,0 +1,28 @@
+library(abind)
+library('oce')
+library(marmap)
+
+# to see a function example:
+example("plot")
+
+plot(1, 2)
+
+a <- 1
+a = 1
+
+1 -> a
+
+# data types: numeric, integer, character,
+# logical (TRUE/FALSE), raw, complex
+
+if (a == 1) {
+    print("a is one!")
+}
+
+for (i in seq_along(vector)) {
+    print(i)
+}
+
+sequence <- seq(1, 10, by=2)
+sequence2 <- seq(1, 10, length.out=100)
+
