921ef6c985ea8af515d039b9762f0c88edc7cd16
Stanley <StanleyR@dfo-mpo.ca>
2019-10-03 17:10:21 -0300

    fix to link to shared

--- 12_R_Mapping/DataSetup.R
+++ 12_R_Mapping/DataSetup.R
@@ -1,16 +1,12 @@
 ## load library ----------
 library(dplyr)
-library(Mar.datawrangling)
 
-## load data ----------
-get_data('rv') #this will look in the data folder. THis assumes your root directory is the bioRworkshops repository
-rvdata <- summarize_catches()
+## load data --------
+rvdata <- read.csv("R:/Shared/R Noobs/RV2010_2018.csv")
 
 #Filter the data for the standard RV survey stratified sets.
-
-rvdata <- filter(rvdata,XTYPE==1)
-
 diversity <- rvdata%>%
+  filter(XTYPE==1)%>%
   group_by(YEAR,MISSION,SETNO)%>%
   summarise(abundance = sum(TOTNO),
             div = n(), # n() is a handy dplyr function that returns the length of the data. Similar to nrow(data.frame) or length(vector)
@@ -26,6 +22,6 @@ plotdata <- rvdata%>%
             filter(XTYPE==1,COMM %in% c("AMERICAN LOBSTER","COD(ATLANTIC)","CUSK"))%>%
             arrange(COMM)%>%
             select(COMM,MISSION,SETNO,YEAR,ends_with("UDE"),TOTWGT,TOTNO)%>%
-            rename(species=COMM,long=ELONGITUDE,lat=ELATITUDE,wgt=TOTWGT,abund=TOTNO) # kind of clean up some of the names
+            rename(species=COMM,long=LONGITUDE,lat=LATITUDE,wgt=TOTWGT,abund=TOTNO) # kind of clean up some of the names
 
 #now plot it and use filter again if you want to subset out any specific species. 
