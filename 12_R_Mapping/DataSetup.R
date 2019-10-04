98bc95090ae43a8e9210dbdf5e331ad1f0f36366
Stanley <StanleyR@dfo-mpo.ca>
2019-10-04 08:26:51 -0300

    fix for the merge (right_join)

--- 12_R_Mapping/DataSetup.R
+++ 12_R_Mapping/DataSetup.R
@@ -6,16 +6,19 @@ rvdata <- read.csv(unz("data/RV2010_2018.zip", "RV2010_2018.csv"), stringsAsFact
 
 #Filter the data for the standard RV survey stratified sets.
 diversity <- rvdata%>%
-  filter(XTYPE==1)%>%
+  filter(XTYPE==1,grepl("NED",.$MISSION))%>%
   group_by(YEAR,MISSION,SETNO)%>%
   summarise(abundance = sum(TOTNO),
-            div = n(), # n() is a handy dplyr function that returns the length of the data. Similar to nrow(data.frame) or length(vector)
-            ldiversity = log10(div))%>%
+            div = n())%>% # n() is a handy dplyr function that returns the length of the data. Similar to nrow(data.frame) or length(vector)
   ungroup()%>% #ungroup because we want the proportion to be within YEAR and not set. 
-  group_by(YEAR,MISSION)%>% #regroup
+  group_by(YEAR)%>% #regroup
   mutate(pdiv=div/max(div,na.rm=T))%>%
   ungroup()%>%
-  right_join(.,rvdata%>%select(ends_with("UDE"),MISSION,SETNO,YEAR),by=c("MISSION","SETNO","YEAR"))%>% #merge back in the coordinates so we can plot
+  right_join(.,rvdata%>%distinct(MISSION,SETNO,.keep_all=T)%>%
+               select(ends_with("UDE"),MISSION,SETNO,YEAR),
+             by=c("MISSION","SETNO","YEAR"))%>% #merge back in the coordinates so we can plot
+  rename(long=LONGITUDE,lat=LATITUDE,year=YEAR)%>%
+  filter(!is.na(div))%>%
   data.frame()
 
 plotdata <- rvdata%>%
